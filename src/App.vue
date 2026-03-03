51da1269c48155121aef0a6c0547048f8bb8d2e6
Inmedia 11 <inmedia.pc.11@gmail.com>
2026-03-03 17:13:15 -0500

    0.0.1

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+    import { RouterLink, RouterView } from 'vue-router'
+    import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+    <!-- <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+    </nav> -->
+
+    <h1>leosoplapuco</h1>
+</template>
